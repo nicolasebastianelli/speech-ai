84f2e28d2a9411ac9c405fb1ad60c7cf20bba512
nicolasebastianelli <nicola.sebastianelli.95@gmail.com>
2023-05-23 12:05:24 +0200

    fixed black

--- tests/utils/test_directory.py
+++ tests/utils/test_directory.py
@@ -5,13 +5,13 @@ from speechai.utils import create_directory_from_path
 
 @pytest.fixture(name="mock_os")
 def fixture_mock_os(mocker):
-    mock = mocker.patch('speechai.utils.directory.os', autospec=True)
+    mock = mocker.patch("speechai.utils.directory.os", autospec=True)
     return mock
 
 
 @pytest.fixture(name="mock_logger")
 def fixture_mock_logger(mocker):
-    return mocker.patch('speechai.utils.directory.logger', autospec=True)
+    return mocker.patch("speechai.utils.directory.logger", autospec=True)
 
 
 def test_create_directory_from_path_existing_directory(mock_os, mock_logger):
@@ -41,6 +41,7 @@ def test_create_directory_from_path_os_error(mock_os, mock_logger):
 
     create_directory_from_path("/new/directory/file.txt")
 
-    mock_logger.error.assert_called_once_with("An error occurred while creating directory. Error details: %s",
-                                              mock_os.makedirs.side_effect)
+    mock_logger.error.assert_called_once_with(
+        "An error occurred while creating directory. Error details: %s", mock_os.makedirs.side_effect
+    )
     mock_os.makedirs.assert_called_once_with("/new/directory")
